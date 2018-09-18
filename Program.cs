ce3e820b5b127c975342cdaaf8e813798bbbbc79
quiddler <>
2018-09-17 21:42:26 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            Console.WriteLine($"Hello {i}!");
+            Console.ReadLine();
+        }
+    }
+}
